e5bc6d4f782f64f691aec8c3061855e91ee6af33
SpencerSolt <spencer.solt@students.snow.edu>
2025-09-30 11:26:21 -0600

    Added a simple form of guessing until the correct word is entered

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 //Spencer Solt, 9/30/25, Mastermind
 Console.WriteLine("Hello, I have chosen 4 letters between 'a' and 'g' and have arranged them in a particular order. Your job is to guess the letters and put them in the right order.");
+string chosenWord = "gbcf";
+string guess;
+do
+{
+    guess = Console.ReadLine();
+}
+while (guess != chosenWord);
+Console.WriteLine("You did it!");
